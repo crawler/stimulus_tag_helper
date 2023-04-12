c0b2bc9b903aa335c054f9aad7f92398cb358582
Anton Topchii <player1@infinitevoid.net>
2023-04-12 20:23:37 +0300

    Ruby LSP vs rubocop conflict

--- test/stimulus_tag_helper/stimulus_action/parse_test.rb
+++ test/stimulus_tag_helper/stimulus_action/parse_test.rb
@@ -15,6 +15,7 @@ module StimulusTagHelper
 
     def test_reference_example1
       action = subject.call("click->gallery#next")
+
       assert_equal(
         StimulusAction.new(event: "click", identifier: "gallery", method: "next"),
         action
@@ -24,6 +25,7 @@ module StimulusTagHelper
 
     def test_reference_event_shorthand
       action = subject.call("gallery#next")
+
       assert_equal(
         StimulusAction.new(identifier: "gallery", method: "next"),
         action
@@ -33,6 +35,7 @@ module StimulusTagHelper
 
     def test_reference_global_events
       action = subject.call("resize@window->gallery#layout")
+
       assert_equal(
         StimulusAction.new(event: "resize", target: "window", identifier: "gallery", method: "layout"),
         action
@@ -42,6 +45,7 @@ module StimulusTagHelper
 
     def test_reference_options1
       action = subject.call("scroll->gallery#layout:!passive")
+
       assert_equal(
         StimulusAction.new(event: "scroll", identifier: "gallery", method: "layout", options: "!passive"),
         action
@@ -51,6 +55,7 @@ module StimulusTagHelper
 
     def test_reference_options2
       action = subject.call("click->gallery#open:capture")
+
       assert_equal(
         StimulusAction.new(event: "click", identifier: "gallery", method: "open", options: "capture"),
         action
@@ -60,6 +65,7 @@ module StimulusTagHelper
 
     def test_parse_with_defaults
       action = subject.call("next", identifier: "gallery")
+
       assert_equal(
         StimulusAction.new(identifier: "gallery", method: "next"),
         action
@@ -69,6 +75,7 @@ module StimulusTagHelper
 
     def test_defaults_can_be_overridden
       action = subject.call("form#next", identifier: "gallery")
+
       assert_equal(
         StimulusAction.new(identifier: "form", method: "next"),
         action
